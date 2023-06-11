0e00923251e1233ca12c99874bbd46d90b0edc94
Marcos Azulay <marcosazulay77@gmail.com>
2023-06-11 12:11:46 -0300

    Hasta acá ya esta funcionnando productos y clientes, se podria presentar como está. Voy a hacer unas mejoras en clientes y asociarlos a productos. Tambien voy a mejorar las vistas de las paginas.

--- apps/producto/urls.py
+++ apps/producto/urls.py
@@ -5,8 +5,15 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('', views.lista_productos, name='lista_productos'),
     path('nuevo_producto/', views.nuevo_producto, name='nuevo_producto'),
-     path('nueva_categoria/', views.nueva_categoria, name='nueva_categoria'),
+    path('nueva_categoria/', views.nueva_categoria, name='nueva_categoria'),
+    path('lista_productos/', views.lista_productos, name='lista_productos'),
+    path('ver_producto/<int:pk>/', views.ver_producto, name='ver_producto'),
+    path('editar_producto/<int:pk>/', views.editar_producto, name='editar_producto'),
+    path('lista_categorias/', views.lista_categorias, name='lista_categorias'),
+    path('editar_categoria/<int:pk>/', views.editar_categoria, name='editar_categoria'),
+    path('borrar_categoria/<int:pk>/', views.borrar_categoria, name='borrar_categoria'),
 
+    
     path('producto/<int:pk>/', views.detalle_producto, name='detalle_producto'),
     path('producto/<int:pk>/editar/', views.editar_producto, name='editar_producto'),
     path('producto/<int:pk>/eliminar/', views.eliminar_producto, name='eliminar_producto'),
--- apps/producto/views.py
+++ apps/producto/views.py
@@ -1,12 +1,29 @@
 # Create your views here.
 
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Producto, Comentario
+from .models import Producto, Categoria
 from .forms import ProductoForm, CategoriaForm, ComentarioForm
+from django.urls import reverse
 
 def lista_productos(request):
+    # Obtener todos los productos
     productos = Producto.objects.all()
-    return render(request, 'productos/lista_productos.html', {'productos': productos})
+
+    # Obtener todas las categorías
+    categorias = Categoria.objects.all()
+
+    # Obtener los parámetros de filtrado
+    categoria_id = request.GET.get('categoria')
+
+    # Aplicar los filtros si se han proporcionado
+    if categoria_id:
+        productos = productos.filter(categoria_id=categoria_id)
+
+    return render(request, 'producto/lista_productos.html', {'productos': productos, 'categorias': categorias})
+
+def lista_categorias(request):
+    categorias = Categoria.objects.all()
+    return render(request, 'producto/lista_categorias.html', {'categorias': categorias})
 
 def nuevo_producto(request):
     if request.method == 'POST':
@@ -25,12 +42,19 @@ def nueva_categoria(request):
     if request.method == 'POST':
         categoria_form = CategoriaForm(request.POST)
         if categoria_form.is_valid():
-            categoria = categoria_form.save()
-            return redirect('/nuevo_producto', id=categoria.id)
+            nombre_categoria = categoria_form.cleaned_data['nombre']
+            if Categoria.objects.filter(nombre=nombre_categoria).exists():
+                # Categoría con el mismo nombre ya existe, mostrar mensaje de error o realizar alguna acción
+                # Por ejemplo, puedes agregar un mensaje de error al formulario y mostrarlo en la plantilla
+                categoria_form.add_error('nombre', 'La categoría ya existe.')
+            else:
+                categoria = categoria_form.save()
+                return redirect('/nuevo_producto', id=categoria.id)
     else:
         categoria_form = CategoriaForm()
 
     return render(request, 'producto/nueva_categoria.html', {'categoria_form': categoria_form})
+
 
 def detalle_producto(request, pk):
     producto = get_object_or_404(Producto, pk=pk)
@@ -42,11 +66,11 @@ def detalle_producto(request, pk):
             comentario = comentario_form.save(commit=False)
             comentario.producto = producto
             comentario.save()
-            return redirect('detalle_producto', pk=pk)
+            return redirect('/detalle_producto', pk=pk)  # Utilizar el nombre de la vista
     else:
         comentario_form = ComentarioForm()
 
-    return render(request, 'productos/detalle_producto.html', {'producto': producto, 'comentarios': comentarios, 'comentario_form': comentario_form})
+    return render(request, 'producto/detalle_producto.html', {'producto': producto, 'comentarios': comentarios, 'comentario_form': comentario_form})
 
 
 def editar_producto(request, pk):
@@ -58,16 +82,42 @@ def editar_producto(request, pk):
             producto = producto_form.save(commit=False)
             producto.usuario = request.user
             producto.save()
-            return redirect('detalle_producto', pk=pk)
+            return redirect('/lista_productos')
     else:
         producto_form = ProductoForm(instance=producto)
 
-    return render(request, 'productos/editar_producto.html', {'producto': producto, 'producto_form': producto_form})
+    return render(request, 'producto/editar_producto.html', {'producto': producto, 'producto_form': producto_form})
 
 def eliminar_producto(request, pk):
     producto = get_object_or_404(Producto, pk=pk)
     if request.method == 'POST':
         producto.delete()
-        return redirect('lista_productos')
+        return redirect('/lista_productos')
     
-    return render(request, 'productos/eliminar_producto.html', {'producto': producto})
+    return render(request, 'producto/eliminar_producto.html', {'producto': producto})
+
+def ver_producto(request, pk):
+    producto = get_object_or_404(Producto, pk=pk)
+    return render(request, 'producto/ver_producto.html', {'producto': producto})
+
+def editar_categoria(request, pk):
+    categoria = get_object_or_404(Categoria, pk=pk)
+
+    if request.method == 'POST':
+        categoria_form = CategoriaForm(request.POST, instance=categoria)
+        if categoria_form.is_valid():
+            categoria_form.save()
+            return redirect('/lista_categorias')
+    else:
+        categoria_form = CategoriaForm(instance=categoria)
+
+    return render(request, 'producto/editar_categoria.html', {'categoria': categoria, 'categoria_form': categoria_form})
+
+def borrar_categoria(request, pk):
+    categoria = get_object_or_404(Categoria, pk=pk)
+
+    if request.method == 'POST':
+        categoria.delete()
+        return redirect('/lista_categorias')
+
+    return render(request, 'producto/borrar_categoria.html', {'categoria': categoria})
